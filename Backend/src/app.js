44d062a12b1808e1ff7fdc308b0e3310eea187f1
zynx0011 <kunalnachan10@gmail.com>
2024-03-18 19:19:16 +0530

    dd

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,8 +12,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
